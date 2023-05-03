c0e8554ce03162690783da87013b20c782f05dd5
Chamnanwit <chamnanwiteamdit@gmail.com>
2023-05-03 17:30:44 +0700

    route login register

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,24 +1,34 @@
-import { createBrowserRouter,RouterProvider } from "react-router-dom"
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { publicRoutes } from './publicRoutes';
 
 function Routes() {
+    const router = createBrowserRouter(publicRoutes);
 
-    const router = createBrowserRouter([
-        {
-            path: '/',
-            element: <h1>Home</h1>
-        },
-        {
-            path: 'profile/:id',
-            element: <h1>profile-friend</h1>
-        },
-        {
-            path: 'about',
-            element: <h1>about</h1>
-        }
-
-    ])
-
-  return <RouterProvider router={router} />;
+    return <RouterProvider router={router} />;
 }
 
-export default Routes
+export default Routes;
+
+// Ex : Array of Router Object 
+// const mockedRotes = [
+//     {
+//         path: '/',
+//         element: <h1>Home</h1>,
+//     },
+//     {
+//         path: 'profile',
+//         element: <h1>profile</h1>,
+//     },
+//     {
+//         path: 'profile/:id',
+//         element: <h1>profile-friend</h1>,
+//     },
+//     {
+//         path: 'about',
+//         element: <h1>about</h1>,
+//     },
+//     {
+//         path: '*',
+//         element: <Navigate to='/' />,
+//     },
+// ];
